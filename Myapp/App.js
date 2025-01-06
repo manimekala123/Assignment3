7f7bea41e26829d7b89176032e96e484312e29bf
manimekala123 <139643976+manimekala123@users.noreply.github.com>
2025-01-06 16:07:12 +0000

    Create the app

---
+++ Myapp/App.js
@@ -0,0 +1,55 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet } from "react-native";
+import { PaperProvider } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Home from "./components/Home";
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <SafeAreaProvider>
+      <PaperProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="login"
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: "#4b0150",
+              },
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                color: "#fff",
+              },
+              headerTitleAlign: "center",
+            }}
+          >
+            <Stack.Screen
+              name="login"
+              component={Login}
+              options={{ title: "UoV Student Care" }}
+            />
+            <Stack.Screen
+              name="home"
+              component={Home}
+              options={{ title: "UoV Student Care" }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
